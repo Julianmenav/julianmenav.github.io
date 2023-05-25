cdd1d9c4a28ed3af69b759fb29424ee96859e3b9
juli <julianmenav2@gmail.com>
2023-05-25 22:19:28 +0200

    style: change aboutme section

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Slider({items}) {
   return (
-    <div className="overflow-hidden h-full flex items-center">
+    <div className="overflow-hidden flex items-center py-8 text-custom-gray bg-[#1a1a1a]">
       <div className="animate-infinite-slide font-extrabold text-3xl">
         {items.map(item => <span className="mx-10 md:mx-20 lg:mx-32">{item}</span>)}
       </div>
