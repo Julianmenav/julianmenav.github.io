0fe38225cc0399d98331fb2a1bd8d104d0bd043b
juli <julianmenav2@gmail.com>
2023-06-03 22:47:23 +0200

    feat: remove hader border & make grid via css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const programmingColors = {
-  Astro: '#ff5d01',
-  Typescript: '#2f74c0',
+  Astro: "#ff5d01",
+  Typescript: "#2f74c0",
   React: "#61dafb",
   Javascript: "#efd81d",
   Tailwind: "#38bdf8",
@@ -11,14 +11,14 @@ const programmingColors = {
   CSS: "#7151a5",
   HTML: "#d84924",
   Laravel: "#f25951",
-  Python: "##f2c73e"
-}
+  Python: "##f2c73e",
+};
 
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'class',
-	theme: {
-		extend: {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  darkMode: "class",
+  theme: {
+    extend: {
       screens: {
         "2xl": "1900px",
         xl: "1400px",
@@ -32,30 +32,34 @@ module.exports = {
         roboto: ["'Roboto Condensed'", "sans-serif"],
       },
       colors: {
-        "dark":"#101010",
+        dark: "#101010",
         "custom-gray": "#777777",
-        "bright-color":"#b09dc2",
-        ...programmingColors
+        "bright-color": "#b09dc2",
+        "grid-color-dark": "#404040",
+        "grid-color-bright": "#d1d1d1",
+        ...programmingColors,
       },
       backgroundImage: {
-        "dot-grid": "url('/dotGrid.svg')",
         "black-white-profile": "url('/img/bw-propic.webp')",
-        "color-profile": "url('/img/color-propic.webp')"
+        "color-profile": "url('/img/color-propic.webp')",
+        "dot-grid-dark": "radial-gradient(circle at 1px 1px, theme('colors.grid-color-dark') 1px, transparent 0)",
+        "dot-grid-bright": "radial-gradient(circle at 1px 1px, theme('colors.grid-color-bright') 1px, transparent 0)",
+      },
+      backgroundSize: {
+        "dot-grid-dark": "27px 27px",
+        "dot-grid-bright": "27px 27px",
       },
       keyframes: {
         slide: {
-          'from': { transform: 'translateX(0)' },
-          'to': { transform: 'translateX(-100%)' } 
-        }
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(-100%)" },
+        },
       },
       animation: {
-        'infinite-slide': 'slide 50s linear infinite' 
+        "infinite-slide": "slide 50s linear infinite",
       },
     },
   },
-  safelist: [
-    ...Object.keys(programmingColors).map(color => `text-${color}`),
-    ...Object.keys(programmingColors).map(color => `border-${color}`)
-  ],
-	plugins: [],
-}
+  safelist: [...Object.keys(programmingColors).map((color) => `text-${color}`), ...Object.keys(programmingColors).map((color) => `border-${color}`)],
+  plugins: [],
+};
