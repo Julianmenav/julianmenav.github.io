5b13fce00cd5034bac4fec447c939b17e87c9ec3
Juli <julianmenav2@gmail.com>
2025-06-07 15:56:18 +0200

    feat: invert white logos color in light mode

---
+++ content/tech-icons.ts
@@ -0,0 +1,105 @@
+export type TechIcon = {
+    name: string;
+    route: string;
+    url: string;
+    invertColorsOnLightMode: boolean;
+}
+
+export const techIcons: TechIcon[] = [
+    {
+        name: "Javascript",
+        route: "/icons/Javascript.svg",
+        url: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Express",
+        route: "/icons/Express.svg",
+        url: "https://expressjs.com/",
+        invertColorsOnLightMode: true
+    },
+    {
+        name: "React",
+        route: "/icons/React.svg",
+        url: "https://react.dev/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Php",
+        route: "/icons/Php.svg",
+        url: "https://www.php.net/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Laravel",
+        route: "/icons/Laravel.svg",
+        url: "https://laravel.com/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Rust",
+        route: "/icons/Ferris.svg",
+        url: "https://www.rust-lang.org/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Git",
+        route: "/icons/Git.svg",
+        url: "https://git-scm.com/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Linux",
+        route: "/icons/Linux.svg",
+        url: "https://www.linux.org/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Tailwind",
+        route: "/icons/Tailwind.svg",
+        url: "https://tailwindcss.com/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Astro",
+        route: "/icons/Astro.svg",
+        url: "https://astro.build/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Python",
+        route: "/icons/Python.svg",
+        url: "https://www.python.org/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Flask",
+        route: "/icons/Flask.svg",
+        url: "https://flask.palletsprojects.com/",
+        invertColorsOnLightMode: true
+    },
+    {
+        name: "AWS",
+        route: "/icons/AWS.svg",
+        url: "https://aws.amazon.com/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Java",
+        route: "/icons/Java.svg",
+        url: "https://java.com/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Spring",
+        route: "/icons/Spring.svg",
+        url: "https://spring.io/",
+        invertColorsOnLightMode: false
+    },
+    {
+        name: "Bulma",
+        route: "/icons/Bulma.svg",
+        url: "https://bulma.io/",
+        invertColorsOnLightMode: false
+    }
+]
